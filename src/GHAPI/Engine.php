22426ddce89d996882ec955b166bcd6f3c62f0f4
Sandor Semsey <semseysandor@gmail.com>
2021-03-05 00:28:35 +0100

    Engine.php

---
+++ src/GHAPI/Engine.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace GHAPI;
+
+/**
+ * Engine Class
+ *
+ * @category Main
+ * @package  ghapi
+ * @author   Sandor Semsey <semseysandor@gmail.com>
+ * @license  MIT https://choosealicense.com/licenses/mit/
+ * php version 8.0
+ */
+class Engine
+{
+    /**
+     * Engine constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Run application
+     */
+    public function run()
+    {
+        echo "TESTING\n";
+    }
+}
---
+++ src/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+// Strict typing
+declare(strict_types=1);
+
+// Autoloader
+include '../vendor/autoload.php';
+
+$engine = new \GHAPI\Engine();
+$engine->run();
