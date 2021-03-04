21cfea7bd523196eea5b2c22c6ae031fcde08d1a
Sandor Semsey <semseysandor@gmail.com>
2021-03-05 00:13:47 +0100

    initial commit

---
+++ bin/build.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#################
+## GHAPI       ##
+##             ##
+## Builds PHAR ##
+#################
+
+# Get project root
+bin_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+project_root="$(dirname "${bin_dir}")"
+
+# Filenames
+php_ex="$(which php)"
+creator="$(which phar-composer)"
+output_file="${project_root}/build/ghapi.phar"
+
+# Create build dir
+mkdir -p "${project_root}/build"
+
+# Remove previous build
+rm -rf ${project_root}/build/*
+
+# Create PHAR
+"${php_ex}" -d phar.readonly=off "${creator}" -q build "${project_root}" "${output_file}"
+
+# Set execute bit
+chmod +x "${output_file}"
+
+echo "Build finished"
+
+exit 0
---
+++ src/Runner.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace GHAPI;
+
+
+class Runner
+{
+    public static function majomlali()
+    {
+        echo "majomkutyabaeca";
+
+    }
+
+}
