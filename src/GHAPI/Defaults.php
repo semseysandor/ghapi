0bb26c546a6107ccdeab3bb81b39cc10c1065f4d
Sandor Semsey <semseysandor@gmail.com>
2021-03-16 18:29:08 +0100

    defaults

---
+++ src/GHAPI/Defaults.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace GHAPI;
+
+/**
+ * Defaults
+ *
+ * @category config
+ * @package  ghapi
+ * @author   Sandor Semsey <sandor@es-progress.hu>
+ * @license  MIT https://choosealicense.com/licenses/mit/
+ * php version 8.0
+ */
+class Defaults
+{
+    /**
+     * Default labels
+     *
+     * @var array
+     */
+    public const LABELS = [
+        [
+            'name' => 'maintenance',
+            'desc' => 'Maintenance or administration related',
+            'color' => '#EDF6BC',
+        ],
+        [
+            'name' => 'automation',
+            'desc' => 'Streamlining work-flows ',
+            'color' => '#E04452',
+        ],
+        [
+            'name' => 'concept',
+            'desc' => 'New system concept, idea',
+            'color' => '#DF38F8',
+        ],
+        [
+            'name' => 'bug',
+            'desc' => 'Something is not working',
+            'color' => '#d73a4a',
+        ],
+        [
+            'name' => 'documentation',
+            'desc' => 'Improvements or additions to documentation ',
+            'color' => '#0075ca',
+        ],
+        [
+            'name' => 'enhancement',
+            'desc' => 'New feature',
+            'color' => '#a2eeef',
+        ],
+        [
+            'name' => 'performance',
+            'desc' => 'It makes the system faster',
+            'color' => '#0052cc',
+        ],
+        [
+            'name' => 'refactoring',
+            'desc' => 'Restructure code, files etc.',
+            'color' => '#077806',
+        ],
+        [
+            'name' => 'security',
+            'desc' => 'Security related improvements',
+            'color' => '#D5B812',
+        ],
+        [
+            'name' => 'stability',
+            'desc' => 'It makes the system more robust ',
+            'color' => '#5319e7',
+        ],
+    ];
+}
